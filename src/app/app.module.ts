69ed964f6230addb7a8733546839a200e831c225
sheyenne de morais carvalho <sheyenne_22@hotmail.com>
2022-02-24 19:33:47 -0400

    angular - parte 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { ContatoComponent } from './views/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
